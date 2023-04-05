84f56b15d66c787d22a48b42bf48486a97bbc9fc
roblopes7 <42593177+roblopes7@users.noreply.github.com>
2023-04-05 10:43:59 -0300

    First Component

--- app/src/main/java/com/udemy/mytipapp/MainActivity.kt
+++ app/src/main/java/com/udemy/mytipapp/MainActivity.kt
@@ -3,29 +3,38 @@ package com.udemy.mytipapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.udemy.mytipapp.components.InputFild
 import com.udemy.mytipapp.ui.theme.MyTipAppTheme
 import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
+@ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                TopHeader()
+                //TopHeader()
+                MainContent()
             }
         }
     }
@@ -41,7 +50,7 @@ fun MyApp(content: @Composable () -> Unit){
         }
     }
 }
-@Preview
+//@Preview
 @Composable
 fun TopHeader(totalPerPerson: Double = 134.9852){
     Surface(modifier = Modifier
@@ -67,7 +76,47 @@ fun TopHeader(totalPerPerson: Double = 134.9852){
         }
     }
 }
-@Preview(showBackground = true)
+
+@ExperimentalComposeUiApi
+@Preview
+@Composable
+fun MainContent(){
+
+    val totalBillState = remember{
+        mutableStateOf("")
+    }
+
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Surface(modifier = Modifier
+        .padding(2.dp)
+        .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp, color = Color.LightGray)
+    ) {
+        Column() {
+            InputFild(
+                valueState = totalBillState,
+                labelId = "Enter Bill",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions{
+                    if(!validState) return@KeyboardActions
+
+                    //TODO onvaluechange
+                    keyboardController?.hide()
+                }
+
+            )
+        }
+    }
+}
+
+//@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyTipAppTheme {
